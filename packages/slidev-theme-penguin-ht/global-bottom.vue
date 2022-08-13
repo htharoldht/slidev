c18ffa0b842eaeaa1bb86c67ca687a2b859c47c4
htharoldht <htharoldht@gmail.com>
2022-08-13 14:14:11 +0800

    style: fix global theme

--- packages/slidev-theme-penguin-ht/global-bottom.vue
+++ packages/slidev-theme-penguin-ht/global-bottom.vue
@@ -1,6 +1,6 @@
 <template>
   <footer v-if="['end', 'none'].indexOf($slidev.nav.currentLayout) === -1">
-    <CornerCurves class="absolute top-0 left-0 transform rotate-90" />
+    <CornerCurves class="corner" />
     <Zigzags v-if="
       [
         'cover',
@@ -11,6 +11,21 @@
         'dynamic-image',
       ].indexOf($slidev.nav.currentLayout) === -1 &&
       $slidev.themeConfigs.decoration !== false
-    " class="absolute top-20 -right-30" />
+    " class="watermark" />
   </footer>
 </template>
+
+<style>
+.corner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: rotate(90deg);
+}
+
+.watermark {
+  position: absolute;
+  top: 5rem;
+  right: -7.5rem;
+}
+</style>
--- packages/slidev-theme-penguin-ht/global-top.vue
+++ packages/slidev-theme-penguin-ht/global-top.vue
@@ -3,34 +3,32 @@ import { isDark } from '@slidev/client/logic/dark'
 </script>
 
 <template>
-  <footer
-    v-if="
-      $slidev.themeConfigs.logo &&
-      ['end'].indexOf($slidev.nav.currentLayout) === -1
-    "
-    class="absolute top-0 right-0 p-2"
-  >
-    <img
-      v-if="isDark"
-      :src="$slidev.themeConfigs.darkLogo"
-      class="h-10 mr-4 z-10"
-    />
-    <img
-      v-if="!isDark"
-      :src="$slidev.themeConfigs.logo"
-      class="h-10 mr-4 z-10"
-    />
+  <footer v-if="
+    $slidev.themeConfigs.logo &&
+    $slidev.nav.currentLayout !== 'end'
+  " class="logo">
+    <img v-if="isDark" :src="$slidev.themeConfigs.darkLogo" class="h-10 mr-4 z-10" />
+    <img v-if="!isDark" :src="$slidev.themeConfigs.logo" class="h-10 mr-4 z-10" />
   </footer>
-  <footer
-    v-if="['end'].indexOf($slidev.nav.currentLayout) === -1"
-    class="absolute bottom-0 right-2 p-2 footer"
-  >
+  <footer v-if="$slidev.nav.currentLayout !== 'end'" class="footer">
     © Powered by HTHaroldHT
   </footer>
 </template>
 
 <style>
+
 .footer {
   font-size: 0.5rem;
+  position: absolute;
+  bottom: 0;
+  right: 0.5rem;
+  padding: 0.5rem;
+}
+
+.logo {
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 0.5rem;
 }
 </style>
