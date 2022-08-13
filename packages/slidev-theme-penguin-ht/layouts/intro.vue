5fd034133f800e3bd4da0a85e6b0bac2b1de3a67
htharoldht <htharoldht@gmail.com>
2022-08-13 14:40:07 +0800

    style: fix theme style

--- packages/slidev-theme-penguin-ht/layouts/intro.vue
+++ packages/slidev-theme-penguin-ht/layouts/intro.vue
@@ -1,26 +1,13 @@
 <template>
   <div class="slidev-layout intro grid h-full">
-    <div class="flex flex-col justify-center text-center mytop">
+    <div class="flex flex-col justify-center text-center my-auto mytop">
       <slot />
     </div>
   </div>
 </template>
 
 <style>
-.slidev-layout.intro {
-  .mytop {
-    transform: translateY(-10%);
-  }
-  h1 {
-    @apply text-6xl leading-20 font-sans font-extrabold;
-  }
-
-  h1 + p {
-    @apply opacity-60 -mt-4 text-2xl;
-  }
-
-  a {
-    @apply text-secondary-400 hover:text-secondary-500 dark:(text-white hover:text-gray-200);
-  }
+.mytop {
+  transform: translateY(-10%);
 }
 </style>
